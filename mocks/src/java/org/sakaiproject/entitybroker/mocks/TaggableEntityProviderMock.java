bc81688574d809ded600181683f20832dcb00006
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-04-13 03:03:10 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-15990 SAK-15990 add ECL-2.0 copyright notice.
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/branches/sakai_2-6-x@59987 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- mocks/src/java/org/sakaiproject/entitybroker/mocks/TaggableEntityProviderMock.java
+++ mocks/src/java/org/sakaiproject/entitybroker/mocks/TaggableEntityProviderMock.java
@@ -1,6 +1,23 @@
-/**
- * TaggableEntityProviderMock.java - entity-broker - 2007 Aug 8, 2007 5:39:49 PM - AZ
- */
+/**********************************************************************************
+ * $URL:$
+ * $Id:$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2008 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
 
 package org.sakaiproject.entitybroker.mocks;
 
