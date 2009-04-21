c9839ad683ad76cfd8be3c08c776c882366f8177
arwhyte@umich.edu <arwhyte@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-04-21 21:22:46 +0000

    SAK-16012 update copyright years
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/branches/sakai_2-6-x@60508 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- utils/src/java/org/sakaiproject/entitybroker/util/http/EasySSLProtocolSocketFactory.java
+++ utils/src/java/org/sakaiproject/entitybroker/util/http/EasySSLProtocolSocketFactory.java
@@ -4,7 +4,7 @@
  * $Date$
  ***********************************************************************************
  *
- * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation
+ * Copyright (c) 2003, 2004, 2005, 2006, 2008, 2009 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
